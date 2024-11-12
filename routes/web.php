1f3514503179d3285b3d30024e01f8d25eddfce0
Melvin <kirigayakazzeno@gmail.com>
2024-11-12 20:55:01 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\AccountController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () { return view('home'); });
+Route::get('/', function () { return view('home'); })->name('home');
 Route::get('/login', function () { return view('login'); })->name('login');
 Route::get('/register', [AccountController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [AccountController::class, 'register']);
